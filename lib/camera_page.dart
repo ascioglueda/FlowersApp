ee8455a30db97a6a90b8c21104b724d68d140a21
ascioglueda <edaascioglu395@gmail.com>
2025-02-21 22:16:23 +0300

    Camera page yapiliyor

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,13 +1,10 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:flowersapp/DetailsPage.dart';
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart'; // doğru import
-import 'package:path/path.dart';
-import 'package:image_picker/image_picker.dart'; // Galeri ve kamera seçimi için
-import 'package:url_launcher/url_launcher.dart'; // Google araması için
+import 'package:http_parser/http_parser.dart';
+import 'dart:convert';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
@@ -17,223 +14,125 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-  final ImagePicker _picker = ImagePicker();
+  File? _image; // Çekilen fotoğrafı tutacak değişken
+  String? predictedResult; // Tahmin sonucunu tutacak değişken
 
-  @override
-  void initState() {
-    super.initState();
-    _initializeCamera();
-  }
-
-
-  // Kamerayı başlatmak için gerekli fonksiyon
-  Future<void> _initializeCamera() async {
-    try {
-      final cameras = await availableCameras();
-      if (cameras.isEmpty) {
-        throw Exception('Kamera bulunamadı.');
-      }
-      final firstCamera = cameras.first;
-
-      _controller = CameraController(
-        firstCamera,
-        ResolutionPreset.high,
-      );
-
-      _initializeControllerFuture = _controller.initialize();
-      setState(() {}); // Kamera başlatıldığında arayüzü güncelle
-    } catch (e) {
-      print('Kamera başlatılamadı: $e');
-    }
-  }
-
-
-  // Fotoğraf çekme ve Flask API'ye gönderme fonksiyonu
-  Future<void> _takePictureAndPredict() async {
-    try {
-      await _initializeControllerFuture;
-
-      // Geçici dosya oluşturuluyor
-      final tempDir = await getTemporaryDirectory();
-      final filePath = '${tempDir.path}/${DateTime.now()}.jpg';
-
-      // Fotoğrafı çek
-      await _controller.takePicture().then((XFile file) async {
-        // Resmi belirtilen dosyaya kaydet
-        await file.saveTo(filePath);
-
-        // Resmi API'ye gönder
-        _predictImage(File(filePath));
-      });
-    } catch (e) {
-      print('Fotoğraf çekme hatası: $e');
-      // Hata durumunda kullanıcıyı bilgilendir
-      _showErrorDialog(context as BuildContext,'Fotoğraf çekilemedi. Lütfen tekrar deneyin.');
-    }
-  }
-
-  // Galeriden fotoğraf seçme
-  Future<void> _pickImageAndPredict() async {
-    try {
-      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-      if (image == null) {
-        _showErrorDialog(context as BuildContext,'Resim seçilmedi.');
-        return;
-      }
-
-      // Resmi API'ye gönder
-      _predictImage(File(image.path));
-    } catch (e) {
-      print('Galeri hatası: $e');
-      _showErrorDialog(context as BuildContext,'Bir hata oluştu. Lütfen tekrar deneyin.');
-    }
-  }
-
-  // Fotoğrafı API'ye gönderme işlemi
-// Fotoğrafı API'ye gönderme işlemi
-  Future<void> _predictImage(File image) async {
-    final uri = Uri.parse('http://172.31.27.60:5000/app');
+  // Fotoğrafı API'ye gönderme fonksiyonu
+  Future<void> sendImageToAPI(File imageFile) async {
+    var uri = Uri.parse('http://172.31.95.61:5000/predict');  // Flask API URL
     var request = http.MultipartRequest('POST', uri);
-    var pic = await http.MultipartFile.fromPath('file', image.path);
-    request.files.add(pic);
 
-    var response = await request.send();
-    if (response.statusCode == 200) {
-      var responseData = await response.stream.bytesToString();
-      var prediction = json.decode(responseData);
+    // Fotoğrafı form-data olarak ekleyin
+    request.files.add(await http.MultipartFile.fromPath(
+      'file',  // 'file' form parameter'ı, Flask tarafındaki 'file' ile eşleşmeli
+      imageFile.path,
+      contentType: MediaType('image', 'jpeg'), // Fotoğrafın içeriği
+    ));
 
-      // Gelen tahmin sonucundan çiçek bilgilerini çıkar
-      String flowerName = prediction['flower_name'] ?? 'Bilinmeyen Çiçek';
+    try {
+      // API'den yanıt al
+      var response = await request.send();
 
-      if (flowerName == 'Bilinmeyen Çiçek') {
-        _showErrorDialog(context as BuildContext, 'Çiçek bulunamadı.');
-        return;
+      // Yanıt alındı
+      if (response.statusCode == 200) {
+        var responseData = await response.stream.bytesToString();
+        var jsonResponse = jsonDecode(responseData);
+        setState(() {
+          predictedResult = jsonResponse['prediction']; // API'den dönen tahmin sonucunu kaydedin
+        });
+        print('Tahmin sonucu: ${jsonResponse['prediction']}');
+      } else {
+        print('Hata: ${response.statusCode}');
+        setState(() {
+          predictedResult = 'Hata oluştu'; // Hata durumunda kullanıcıya bilgi verin
+        });
       }
-
-      String description = prediction['description'] ?? 'Açıklama bulunamadı.';
-      String habitat = prediction['habitat'] ?? 'Habitat bilgisi yok.';
-      String light = prediction['light'] ?? 'Işık gereksinimi bilinmiyor.';
-      String soil = prediction['soil'] ?? 'Toprak türü bilinmiyor.';
-      String watering = prediction['watering'] ?? 'Sulama bilgisi yok.';
-
-      // Sonucu göster
-      Navigator.push(
-        context as BuildContext,
-        MaterialPageRoute(
-          builder: (context) => DetailsPage(
-            flowerName: flowerName,
-            description: description,
-            habitat: habitat,
-            light: light,
-            soil: soil,
-            watering: watering,
-          ),
-        ),
-      );
-    } else {
-      _showErrorDialog(context as BuildContext, 'Tahmin alınamadı.');
-    }
-  }
-  // Hata mesajı göster
-  void _showErrorDialog(BuildContext context, String message) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Hata'),
-          content: Text(message),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Tamam'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-
-  // Google'da arama yapma
-  Future<void> _searchGoogle(String query) async {
-    final url = Uri.parse('https://www.google.com/search?q=$query');
-    if (await canLaunch(url.toString())) {
-      await launch(url.toString());
-    } else {
-      print('Google araması açılamadı.');
+    } catch (e) {
+      print('Bağlantı Hatası: $e');
+      setState(() {
+        predictedResult = 'API bağlantısı sağlanamadı'; // Hata durumunda kullanıcıya bilgi verin
+      });
     }
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  // Kamera izni kontrolü ve fotoğraf çekme işlemi
+  Future<void> _takePhoto() async {
+    var status = await Permission.camera.request();
+    if (status.isGranted) {
+      final ImagePicker _picker = ImagePicker();
+      final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+
+      if (photo != null) {
+        setState(() {
+          _image = File(photo.path); // Fotoğrafı _image değişkenine kaydet
+        });
+      } else {
+        print("Fotoğraf çekilmedi.");
+      }
+    } else {
+      print("Kamera izni verilmedi.");
+    }
+  }
+
+  Future<void> _pickImageFromGallery() async {
+    var status = await Permission.storage.request();
+    if (status.isGranted) {
+      final ImagePicker _picker = ImagePicker();
+      final XFile? photo = await _picker.pickImage(source: ImageSource.gallery);
+
+      if (photo != null) {
+        setState(() {
+          _image = File(photo.path); // Seçilen fotoğrafı _image değişkenine kaydet
+        });
+      } else {
+        print("Fotoğraf seçilmedi.");
+      }
+    } else {
+      print("Galeriden fotoğraf seçme izni reddedildi.");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Kamera ve Galeri'),
-        backgroundColor: Colors.green.shade300,
+        title: Text('Kamera Sayfası'),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FutureBuilder<void>(
-                future: _initializeControllerFuture,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    return CameraPreview(_controller);
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Fotoğraf varsa göster, yoksa mesaj göster
+            _image == null
+                ? Text('Fotoğraf seçilmedi.')
+                : Image.file(_image!), // Seçilen veya çekilen fotoğrafı göster
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _takePhoto,
+              child: Text('Kamera ile Fotoğraf Çek'),
+            ),
+            ElevatedButton(
+              onPressed: _pickImageFromGallery,
+              child: Text('Galeriden Fotoğraf Seç'),
+            ),
+            // Fotoğraf seçildikten sonra "Gönder" butonu ekleyin
+            if (_image != null)
+              ElevatedButton(
+                onPressed: () {
+                  sendImageToAPI(_image!); // Fotoğrafı API'ye gönder
                 },
+                child: Text('Gönder'),
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _takePictureAndPredict,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green.shade800,
-                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                ),
-                child: const Text(
-                  'Fotoğraf Çek ve Tahmin Al',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
+            // API'den gelen tahmin sonucunu göster
+            if (predictedResult != null)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Tahmin Sonucu: $predictedResult',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _pickImageAndPredict,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green.shade800,
-                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                ),
-                child: const Text(
-                  'Galeriden Resim Seç ve Tahmin Al',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () => _searchGoogle('Resim bulunamadı'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red.shade800,
-                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                ),
-                child: const Text(
-                  'Google\'da Ara',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-              ),
-            ],
-          ),
+          ],
         ),
       ),
     );
